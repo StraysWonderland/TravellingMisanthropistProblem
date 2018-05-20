013c1c26fa42ee675cd0c746a8b25a70fa93110a
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-05-20 23:29:03 +0200

    added distance approximation method

---
+++ src/main/java/Util/Distance.java
@@ -0,0 +1,20 @@
+package Util;
+
+public  class Distance {
+
+    /**
+     * @param lat
+     *            Latitude of  each node.
+     * @param lng
+     *            Longitude of each node.
+     * @return Approximation of the distance between two points.
+     *
+     *         approximate distance between two points using euclidean distance.
+     *         Error of approximation negligible for short distances
+     */
+    private static double calculateDistance(double latNode1, double lngNode1, double latNode2, double lngNode2) {
+        double x = latNode1 - latNode2;
+        double y = (lngNode1 - lngNode2) * Math.cos(latNode2);
+        return Math.sqrt(x * x + y * y) * 110.25;
+    }
+}
