84fe78040894d4533f9aa0c487a05055062d2be3
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-05-22 19:53:20 +0200

    fixed edge retrieval
    - minor refactoring
    - some comments added

--- src/main/java/GraphStructure/GraphManager.java
+++ src/main/java/GraphStructure/GraphManager.java
@@ -22,9 +22,10 @@ public class GraphManager {
     private int[][] edges;
     private int[] offset;
 
-    private List<double[]> edgeList = new ArrayList<>();
-    private List<String> barsList = new ArrayList<>();
     HashMap<Long, double[]> nodeLookup;
+
+    private int wayCount = 0;
+    private int nodeCount = 0;
 
     // nodes currently : 2969443
     private int numNodes = 39472043;
@@ -34,10 +35,9 @@ public class GraphManager {
     InputStream stream;
 
     public GraphManager() {
-        edges = new int[3][numEdges];
-        nodes = new double[3][numNodes];
         offset = new int[numNodes];
         Arrays.fill(offset, -1);
+
         nodeLookup = new HashMap<>();
     }
 
@@ -54,6 +54,15 @@ public class GraphManager {
             stream = new FileInputStream(pbfPath);
             localiseAndSortNodes();
             System.out.println("localised nodes");
+
+            stream = new FileInputStream(pbfPath);
+            retrieveEdgesBetweenNodes();
+            sortEdges();
+            System.out.println("retrieved edges between all relevant nodes");
+
+            System.out.println(nodeCount);
+            System.out.println(wayCount);
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -77,8 +86,10 @@ public class GraphManager {
             if (container.getType() == EntityType.Way) {
                 OsmWay currentEdge = (OsmWay) container.getEntity();
                 if (Highway.isHighway(OsmModelUtil.getTagsAsMap(currentEdge).get("highway"))) {
+                    wayCount++;
                     for (int i = 0; i < currentEdge.getNumberOfNodes(); i++) {
                         nodeLookup.put(currentEdge.getNodeId(i), new double[]{});
+                        nodeCount++;
                         System.out.println("relevant node added: " + currentEdge.getNodeId(i));
                     }
                 }
@@ -107,12 +118,13 @@ public class GraphManager {
     }
 
     private void localiseAndSortNodes() {
+        nodes = new double[3][nodeCount];
         List<double[]> nodeList = new ArrayList<>(nodeLookup.values());
         Collections.sort(nodeList, (a, b) -> (Double.compare(a[0], b[0])));
         for (int i = 0; i < nodeList.size(); i++) {
             nodes[0][i] = nodeList.get(i)[1];   // latitude
             nodes[1][i] = nodeList.get(i)[2];   // longitude
-
+            // store id-mapping in lookup-table
             nodeLookup.put((long) nodeList.get(i)[0],
                     new double[]{
                             (double) i, // serves as localId
@@ -124,6 +136,7 @@ public class GraphManager {
     }
 
     private void retrieveEdgesBetweenNodes() {
+        edges = new int[3][wayCount];
         iterator = new PbfIterator(stream, false);
         for (EntityContainer container : iterator) {
             if (container.getType() == EntityType.Way) {
@@ -139,12 +152,12 @@ public class GraphManager {
 
     private void convertToEdgeStructure(OsmWay way) {
         for (int i = 0; i < way.getNumberOfNodes() - 1; i++) {
-            double[] node1 = nodeLookup.get(i);
-            double[] node2 = nodeLookup.get(i + 1);
+            double[] node1 = nodeLookup.get(way.getNodeId(i));
+            double[] node2 = nodeLookup.get(way.getNodeId(i+1));
 
-            edges[i][0] = (int) node1[0];
-            edges[i][1] = (int) node2[0];
-            edges[i][2] = (int) Distance.calculateDistance(node1[1], node1[2], node2[1], node2[2]);
+            edges[0][i] = (int) node1[0]; // starting node
+            edges[1][i] = (int) node2[0]; // target node
+            edges[2][i] = (int) Distance.calculateDistance(node1[1], node1[2], node2[1], node2[2]); //distance
         }
     }
 
