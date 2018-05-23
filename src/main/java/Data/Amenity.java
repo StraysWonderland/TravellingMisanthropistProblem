e6e75b18901e4cdabd4a8446bd8c2d5c8e02329a
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-05-23 22:31:38 +0200

    added basic class for amenity-checks

---
+++ src/main/java/Data/Amenity.java
@@ -0,0 +1,25 @@
+package Data;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Amenity {
+
+    private static List<String> desiredAmenitiesValues = Arrays.asList(
+            "pub",
+            "bar",
+            "biergarten",
+            "cafe",
+            "brothel",
+            "gambling",
+            "nightclub",
+            "stripclub",
+            "swingerclub",
+            "restaurant"
+            );
+
+    public static Set<String> desiredAmenities = new HashSet<>(desiredAmenitiesValues);
+
+}
