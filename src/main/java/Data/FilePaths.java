1aca21da022d9c305631345a995a0abb3be4b809
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-06-16 21:57:40 +0200

    adjusted FIlePaths to distinguish between edges and nodes

--- src/main/java/Data/FilePaths.java
+++ src/main/java/Data/FilePaths.java
@@ -3,8 +3,12 @@ package Data;
 public class FilePaths {
 
     public static final String pbfBW= "bw.osm.pbf";
-    public static final String binBW = "bw.osm.binary";
+    public static final String binBWNodes = "bw.osm.nodes";
+    public static final String binBWEdges = "bw.osm.edges";
+    public static final String binBWOffsets = "bw.osm.offsets";
 
     public static final String pbfGermany = "ger.osm.pbf";
-    public static final String binGermany = "ger.osm.binary";
+    public static final String binGermanyNodes = "ger.osm.nodes";
+    public static final String binGermanyEdges = "ger.osm.edges";
+    public static final String binGermanyOffsets = "ger.osm.offsets";
 }
