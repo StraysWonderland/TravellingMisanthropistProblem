fa7960bc85a060c41ee1aa7c6c6a525b3ec213e0
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-07-11 14:38:53 +0200

    added some utility functions for parsing

---
+++ src/main/java/Util/PathTypes.java
@@ -0,0 +1,26 @@
+package Util;
+
+import DTO.Highway;
+import de.topobyte.osm4j.core.model.iface.OsmTag;
+import de.topobyte.osm4j.core.model.iface.OsmWay;
+import de.topobyte.osm4j.core.model.util.OsmModelUtil;
+
+import java.util.Map;
+
+public class PathTypes {
+
+    public static boolean isOneWay(OsmWay osmWay) {
+        Map<String, String> tags = OsmModelUtil.getTagsAsMap(osmWay);
+        return tags.get("oneway").equals("yes") || tags.containsValue("motorway") || tags.containsValue("motorway_link");
+    }
+
+    public static float[] getMaxSpeed(OsmWay osmWay) {
+        Map<String, String> tags = OsmModelUtil.getTagsAsMap(osmWay);
+        String highway = tags.get("highway");
+        float type = Highway.valueOf(highway).getType();
+        float maxSpeed = Highway.valueOf(highway).getMaxSpeed();
+        return new float[]{type, maxSpeed};
+    }
+
+
+}
