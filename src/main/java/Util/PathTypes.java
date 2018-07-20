26e32f90687db30431b748334295e6d17eac045b
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-07-20 23:14:27 +0200

    changes to pathType :angry:

--- src/main/java/Util/PathTypes.java
+++ src/main/java/Util/PathTypes.java
@@ -1,7 +1,6 @@
 package Util;
 
 import DTO.Highway;
-import de.topobyte.osm4j.core.model.iface.OsmTag;
 import de.topobyte.osm4j.core.model.iface.OsmWay;
 import de.topobyte.osm4j.core.model.util.OsmModelUtil;
 
@@ -9,9 +8,20 @@ import java.util.Map;
 
 public class PathTypes {
 
-    public static boolean isOneWay(OsmWay osmWay) {
+    public static boolean isOneWayTag(OsmWay osmWay) {
         Map<String, String> tags = OsmModelUtil.getTagsAsMap(osmWay);
-        return tags.get("oneway").equals("yes") || tags.containsValue("motorway") || tags.containsValue("motorway_link");
+        return tags.containsKey("oneway") && tags.get("oneway").equals("yes") || tags.containsValue("motorway") || tags.containsValue("motorway_link");
+    }
+
+    public static boolean isOneWay(OsmWay osmWay) {
+        boolean oneWay = false;
+        for (int j = 0; j < osmWay.getNumberOfTags(); j++) {
+            oneWay = (osmWay.getTag(j).getKey().equals("oneway")
+                    && osmWay.getTag(j).getValue().equals("yes"))
+                    || osmWay.getTag(j).getKey().equals("motorway")
+                    || osmWay.getTag(j).getKey().equals("motorway_link");
+        }
+        return true;
     }
 
     public static float[] getMaxSpeed(OsmWay osmWay) {
