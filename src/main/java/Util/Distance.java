0c959b44cb6c16094fe1428496afbf94fc69b771
Cagri Tasci <cagri.c.tasci@outlook.de>
2018-08-06 21:12:50 +0200

    refactoring to distance class

--- src/main/java/Util/Distance.java
+++ src/main/java/Util/Distance.java
@@ -12,7 +12,7 @@ public class Distance {
      * approximate distance between two points using euclidean distance.
      * Error of approximation negligible for short distances
      */
-    public static double calculateDistance(double latNode1, double lngNode1, double latNode2, double lngNode2) {
+    public static double euclideanDistance(double latNode1, double lngNode1, double latNode2, double lngNode2) {
         double x = latNode1 - latNode2;
         double y = (lngNode1 - lngNode2) * Math.cos(latNode2);
         return Math.sqrt(x * x + y * y) * 110.25;
