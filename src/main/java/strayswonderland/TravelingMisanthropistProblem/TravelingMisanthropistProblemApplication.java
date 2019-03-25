1d382f7ca4d94918543faff2678758ffe2473f77
Cagri Tasci <cagri.c.tasci@outlook.de>
2019-03-25 17:54:59 +0100

    changes to jar generation

--- src/main/java/strayswonderland/TravelingMisanthropistProblem/TravelingMisanthropistProblemApplication.java
+++ src/main/java/strayswonderland/TravelingMisanthropistProblem/TravelingMisanthropistProblemApplication.java
@@ -1,6 +1,7 @@
 package strayswonderland.TravelingMisanthropistProblem;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
