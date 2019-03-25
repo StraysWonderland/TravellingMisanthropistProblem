06f4f60d7e78351119f3a33a81e9db0c317f38b4
Cagri Tasci <cagri.c.tasci@outlook.de>
2019-03-25 17:54:28 +0100

    cleaned and restructured graph
    changed nodeGrid for memory efficiency

--- src/main/java/strayswonderland/TravelingMisanthropistProblem/GraphStructure/Graph.java
+++ src/main/java/strayswonderland/TravelingMisanthropistProblem/GraphStructure/Graph.java
@@ -10,79 +10,29 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Graph {
-    private HashMap<String, LinkedList<Integer>> nodeGrid;
-    private HashMap<String, LinkedList<Integer>> amenityGrid;
+
+    private HashMap<Integer, int[]> nodeGrid;
 
     final String edgesPath = FilePaths.binBWEdges;
     String nodesPath = FilePaths.binBWNodes;
 
-   public int[] edgeSource;
-   public int[] edgeTarget;
-   public int[] edgeDistance;
+    public int[] edgeSource;
+    public int[] edgeTarget;
+    public int[] edgeDistance;
 
     public double[][] nodes;
     public int[] offsets;
 
-    int amenityCount = 0;
-    private double[][] amenities;
-
-    private void createNodeGrid() {
-        nodeGrid = new HashMap<>();
-        createGrid(nodes, nodeGrid);
-    }
-
-    private void createAmenityGrid() {
-        amenityGrid = new HashMap<>();
-        createGrid(amenities, amenityGrid);
-    }
-
-    private void createGrid(double[][] targetNodes, HashMap<String, LinkedList<Integer>> targetGrid) {
-        for (int i = 0; i < targetNodes[0].length; i++) {
-            String gridKey = (double) Math.round(targetNodes[0][i] * 10) / 10 + "-"
-                    + (double) Math.round(targetNodes[1][i] * 10) / 10;
-            if (targetGrid.containsKey(gridKey)) {
-                targetGrid.get(gridKey).add(i);
-            } else {
-                LinkedList<Integer> nodesInCurrentCell = new LinkedList<>();
-                nodesInCurrentCell.add(i);
-                targetGrid.put(gridKey, nodesInCurrentCell);
-            }
-        }
-    }
-
-    public int getNearestNode(double[] latLng) {
-        int nearestNodeIndex = -1;
-        double shortestDist = Double.MAX_VALUE;
-        String gridKey = (double) Math.round(latLng[0] * 10) / 10 + "-" + (double) Math.round(latLng[1] * 10) / 10;
-        LinkedList<Integer> gridCell;
-
-        if (!nodeGrid.containsKey(gridKey))
-            return -1;
-
-        gridCell = nodeGrid.get(gridKey);
-
-        for (int nodeID : gridCell) {
-            double dist = Distance.euclideanDistance(nodes[0][nodeID], nodes[1][nodeID], latLng[0], latLng[1]);
-            if (dist < shortestDist) {
-                shortestDist = dist;
-                nearestNodeIndex = nodeID;
-            }
-        }
-        return nearestNodeIndex;
-    }
-
-    public double[] getNodeCoords(int index) {
-        return new double[]{nodes[0][index], nodes[1][index]};
-    }
-
     public void loadMapData() {
         try {
             loadNodes();
+            System.out.println("finished reading nodes");
             loadEdges();
-            createNodeGrid();
+            System.out.println("finished reading edges");
+            generateNodeGrid();
+            System.out.println("created NodeGrid");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("REQUIRED .NODES AND .EDGES FILES COULD NOT BE FOUND: MAKE SURE TO RUN THE PARSER FIRST");
@@ -146,22 +96,111 @@ public class Graph {
         bf.close();
     }
 
+  /*  private void createNodeGrid() {
+        nodeGrid = new HashMap<>();
+        for (int i = 0; i < nodes[0].length; i++) {
+            String gridKey = generateKey(nodes[0][i], nodes[1][i]);
+            if (nodeGrid.containsKey(gridKey)) {
+                nodeGrid.get(gridKey).add(i);
+            } else {
+                System.out.println("new grid-key added");
+                LinkedList<Integer> nodesInCurrentCell = new LinkedList<>();
+                nodesInCurrentCell.add(i);
+                nodeGrid.put(gridKey, nodesInCurrentCell);
+            }
+        }
+    }*/
 
-    public int[] getEdgeSource() {
-        return edgeSource;
-    }
-    public int[] getEdgeTarget() {
-        return edgeTarget;
-    }
-    public int[] getEdgeDistance() {
-        return edgeDistance;
+   /* public int getNearestNode(double[] latLng) {
+        String gridKey = generateKey(latLng[0], latLng[1]);
+        if (!nodeGrid.containsKey(gridKey))
+            return -1;
+
+        List<Integer> gridCell = nodeGrid.get(gridKey);
+        int nearestNodeIndex = -1;
+        double shortestDist = Double.MAX_VALUE;
+
+        for (int nodeID : gridCell) {
+            double dist = Distance.euclideanDistance(nodes[0][nodeID], nodes[1][nodeID], latLng[0], latLng[1]);
+            if (dist < shortestDist) {
+                shortestDist = dist;
+                nearestNodeIndex = nodeID;
+            }
+        }
+        return nearestNodeIndex;
+    }*/
+
+    public double[] getNodeCoords(int index) {
+        return new double[]{nodes[0][index], nodes[1][index]};
     }
 
-    public double[][] getNodes() {
-        return nodes;
+    private String generateKey(double lat, double lng) {
+        StringBuilder gridKey = new StringBuilder();
+        gridKey.append((double) Math.round(lat * 10) / 10);
+        gridKey.append("-");
+        gridKey.append((double) Math.round(lng * 10) / 10);
+
+        return gridKey.toString();
     }
 
-    public int[] getOffsets() {
-        return offsets;
+
+    private void generateNodeGrid() {
+        long startTime = System.currentTimeMillis();
+        nodeGrid = new HashMap<>();
+        // Initialize nodeGridDimensions, which contains dimensions for each key in nodeGrid to set its array size
+        HashMap<Integer, Integer> nodeGridDimensions = new HashMap<>();
+        for (int i = 0; i < nodes[0].length; i++) {
+            int nodeKey = encodeCoordinates(i);
+            nodeGridDimensions.computeIfAbsent(nodeKey, (x -> 1));
+            nodeGridDimensions.computeIfPresent(nodeKey, (k, x) -> (x + 1));
+        }
+        // The current highest non-empty position of the array is stored at the first cell of the array itself
+        for (int i = 0; i < nodes[0].length; i++) {
+            int nodeKey = encodeCoordinates(i);
+            if (!nodeGrid.containsKey(nodeKey)) {
+                nodeGrid.computeIfAbsent(nodeKey, (x -> new int[nodeGridDimensions.get(nodeKey)]))[1] = i;
+                nodeGrid.get(nodeKey)[0] += 1;
+            } else {
+                nodeGrid.get(nodeKey)[nodeGrid.get(nodeKey)[0]] = i;
+                nodeGrid.get(nodeKey)[0] += 1;
+            }
+        }
+        System.out.println("Grid generated in: " + (System.currentTimeMillis() - startTime) + " ms");
+    }
+
+    private int encodeCoordinates(int current) {
+        int latitude = (int) nodes[0][current];
+        int longitude = (int) nodes[1][current];
+        String s = String.valueOf(latitude) + String.valueOf(longitude);
+        return Integer.valueOf(s);
+    }
+
+    public int getNearestNode(double[] markerCoordinates) {
+        int latSource = (int) markerCoordinates[0];
+        int lonSource = (int) markerCoordinates[1];
+        String s = "" + latSource + lonSource;
+        int nodeKey = Integer.valueOf(s);
+
+        //initialize distance-array with length - 1 because the gridArray's first element is not a node but an information
+        //that was needed to fill it in the graph.class.
+        int nearestNeighbourId = Integer.MAX_VALUE;
+        if (nodeGrid.containsKey(nodeKey)) {
+            double[] distanceNeighboursFromSource = new double[nodeGrid.get(nodeKey).length - 1];
+            nearestNeighbourId = 0;
+            double nearestNeighbourCurrent = Double.MAX_VALUE;
+
+            for (int j = 0; j < distanceNeighboursFromSource.length; j++) {
+                distanceNeighboursFromSource[j] = Distance.euclideanDistance(markerCoordinates[0],
+                        markerCoordinates[1],
+                        nodes[0][nodeGrid.get(nodeKey)[j + 1]],
+                        nodes[1][nodeGrid.get(nodeKey)[j + 1]]);
+
+                if (distanceNeighboursFromSource[j] < nearestNeighbourCurrent) {
+                    nearestNeighbourCurrent = distanceNeighboursFromSource[j];
+                    nearestNeighbourId = (nodeGrid.get(nodeKey)[j + 1]);
+                }
+            }
+        }
+        return nearestNeighbourId;
     }
 }
